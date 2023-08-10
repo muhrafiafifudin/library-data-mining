597606edcd0c438e2906002987863c13ba0f8f02
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-08-10 08:52:26 +0700

    Change sidebar & edit view login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -38,7 +38,6 @@
                         </div>
                         <div class="form-group">
                             <label for="password" class="placeholder"><b>Password</b></label>
-                            <a href="#" class="link float-right">Forget Password ?</a>
                             <div class="position-relative">
                                 <input id="password" name="password" type="password" class="form-control" required>
                                 <div class="show-password">
@@ -46,19 +45,8 @@
                                 </div>
                             </div>
                         </div>
-                        <div class="form-group form-action-d-flex mb-3">
-                            <div class="custom-control custom-checkbox">
-                                <input type="checkbox" class="custom-control-input" id="rememberme">
-                                <label class="custom-control-label m-0" for="rememberme">Remember Me</label>
-                            </div>
+                        <div class="form-group mb-3 mt-3">
                             <button type="submit" class="btn btn-primary col-md-5 float-right mt-3 mt-sm-0 fw-bold">Sign In</button>
-                        </div>
-                        <!-- 				<div class="form-action">
-                            <a href="#" class="btn btn-primary btn-rounded btn-login">Sign In</a>
-                        </div> -->
-                        <div class="login-account">
-                            <span class="msg">Don't have an account yet ?</span>
-                            <a href="#" id="show-signup" class="link">Sign Up</a>
                         </div>
                     </div>
                 </form>
