76cd76fffcd33dae7c91976f5b4ae933ab63cfad
Muhammad Rafi A <muhammad.rafi.afifudin99@gmail.com>
2023-08-30 09:51:23 +0700

    Update view login

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -25,7 +25,7 @@
     <body class="login">
         <div class="wrapper wrapper-login">
             <div class="container container-login animated fadeIn">
-                <h3 class="text-center">Sign In To Admin</h3>
+                <h3 class="text-center">Masuk ke Sistem</h3>
 
                 <form action="{{ route('login') }}" method="POST">
                     @csrf
@@ -46,56 +46,10 @@
                             </div>
                         </div>
                         <div class="form-group mb-3 mt-3">
-                            <button type="submit" class="btn btn-primary col-md-5 float-right mt-3 mt-sm-0 fw-bold">Sign In</button>
+                            <button type="submit" class="btn btn-primary col-md-5 float-right mt-3 mt-sm-0 fw-bold">Masuk</button>
                         </div>
                     </div>
                 </form>
-            </div>
-
-            <div class="container container-signup animated fadeIn">
-                <h3 class="text-center">Sign Up</h3>
-                <div class="login-form">
-                    <div class="form-group">
-                        <label for="fullname" class="placeholder"><b>Fullname</b></label>
-                        <input  id="fullname" name="fullname" type="text" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email" class="placeholder"><b>Email</b></label>
-                        <input  id="email" name="email" type="email" class="form-control" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="passwordsignin" class="placeholder"><b>Password</b></label>
-                        <div class="position-relative">
-                            <input  id="passwordsignin" name="passwordsignin" type="password" class="form-control" required>
-                            <div class="show-password">
-                                <i class="flaticon-interface"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label for="confirmpassword" class="placeholder"><b>Confirm Password</b></label>
-                        <div class="position-relative">
-                            <input  id="confirmpassword" name="confirmpassword" type="password" class="form-control" required>
-                            <div class="show-password">
-                                <i class="flaticon-interface"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="row form-sub m-0">
-                        <div class="custom-control custom-checkbox">
-                            <input type="checkbox" class="custom-control-input" name="agree" id="agree">
-                            <label class="custom-control-label" for="agree">I Agree the terms and conditions.</label>
-                        </div>
-                    </div>
-                    <div class="row form-action">
-                        <div class="col-md-6">
-                            <a href="#" id="show-signin" class="btn btn-danger btn-link w-100 fw-bold">Cancel</a>
-                        </div>
-                        <div class="col-md-6">
-                            <a href="#" class="btn btn-primary w-100 fw-bold">Sign Up</a>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
         <script src="{{ asset('assets/js/core/jquery.3.2.1.min.js') }}"></script>
